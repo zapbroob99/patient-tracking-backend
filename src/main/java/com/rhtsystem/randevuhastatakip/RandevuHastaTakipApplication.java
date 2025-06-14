f8b602c15f320531784335d60aabf6619d32d4da
zapbroob99 <zapbroob@gmail.com>
2025-06-15 00:49:43 +0300

    first commit

---
+++ src/main/java/com/rhtsystem/randevuhastatakip/RandevuHastaTakipApplication.java
@@ -0,0 +1,13 @@
+package com.rhtsystem.randevuhastatakip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandevuHastaTakipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandevuHastaTakipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhtsystem/randevuhastatakip/RandevuHastaTakipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhtsystem.randevuhastatakip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandevuHastaTakipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
